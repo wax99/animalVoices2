cbdbd168c34cf4986ff83c082190f45e9dea162e
mizz <mitolovec@gmail.com>
2018-02-03 23:36:37 +0100

    Initial Commit

---
+++ animalVoices2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animalVoices2
+//
+//  Created by Mitja Bradaskja on 28/01/2018.
+//  Copyright © 2018 Mitja Bradaskja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
